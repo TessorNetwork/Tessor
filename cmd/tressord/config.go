650242d2dbb385c2b6ca63c46445dd3094a6a756
nimrostafarian <adrian2ross@gmail.com>
2023-01-19 00:26:47 +0530

    commit

--- cmd/tressord/config.go
+++ cmd/tressord/config.go
@@ -4,7 +4,6 @@ import (
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	"github.com/cosmos/cosmos-sdk/types/address"
 	sdkerrors "github.com/cosmos/cosmos-sdk/types/errors"
-	"github.com/cosmos/cosmos-sdk/version"
 )
 
 const (
